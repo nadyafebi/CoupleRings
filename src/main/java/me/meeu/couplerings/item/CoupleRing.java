c427da282ae431bcc3cc8369539311dd189ae7ee
Nadya Djojosantoso <hello@nadyafebi.com>
2025-02-15 01:33:09 +0700

    Add tooltip on the ring to show couple's names

--- src/main/java/me/meeu/couplerings/item/CoupleRing.java
+++ src/main/java/me/meeu/couplerings/item/CoupleRing.java
@@ -7,12 +7,15 @@ import org.jetbrains.annotations.NotNull;
 
 import me.meeu.couplerings.component.PlayerComponent;
 import me.meeu.couplerings.init.ModDataComponents;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 
 public class CoupleRing extends Item {
@@ -40,5 +43,32 @@ public class CoupleRing extends Item {
         }
 
         return super.use(level, player, usedHand);
+    }
+
+    @Override
+    public void appendHoverText(@NotNull ItemStack stack, @NotNull TooltipContext context,
+            @NotNull List<Component> tooltipComponents,
+            @NotNull TooltipFlag tooltipFlag) {
+        String tooltip = "Not Set";
+
+        Level level = context.level();
+        if (level == null)
+            return;
+
+        if (stack.has(ModDataComponents.PLAYER_ONE)) {
+            String playerUuid = stack.get(ModDataComponents.PLAYER_ONE).uuid();
+            Player player = level.getPlayerByUUID(UUID.fromString(playerUuid));
+            String playerName = player != null ? player.getName().getString() : playerUuid;
+            tooltip = playerName;
+        }
+
+        if (stack.has(ModDataComponents.PLAYER_TWO)) {
+            String playerUuid = stack.get(ModDataComponents.PLAYER_ONE).uuid();
+            Player player = level.getPlayerByUUID(UUID.fromString(playerUuid));
+            String playerName = player != null ? player.getName().getString() : playerUuid;
+            tooltip += " & " + playerName;
+        }
+
+        tooltipComponents.add(Component.literal(tooltip).withStyle(ChatFormatting.ITALIC, ChatFormatting.LIGHT_PURPLE));
     }
 }
